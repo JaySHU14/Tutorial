c6e58399d86a19e049f78746000b00471dacaedd
JaySHU14 <145919670+JaySHU14@users.noreply.github.com>
2023-09-24 20:57:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string user = "Anabella";
+Console.WriteLine("Hello" + user + "!");
+Console.WriteLine($"Hello, {user}!");
+
+Console.WriteLine("What is your name?");
+string user1 = Console.ReadLine();
+Console.WriteLine($"Hello, {user}!");
+
+string defaultName = "Anonymous";
+Console.WriteLine("What is your name?");
+string user2 = Console.ReadLine();
+user2 = user2.Trim().Length == 0 ? defaultName : user2;
+Console.WriteLine($"Hello, {user}!");
+
